054aaed45a4a4a0c8303773043ca12e24e5598d2
Nazaret <nazaretmiranda@gmail.com>
2019-07-12 09:43:14 +0100

    Modifed editProduct method to prevent null value properties from being updated to the original product

--- src/main/java/com/empresa/product/service/ProductServiceImp.java
+++ src/main/java/com/empresa/product/service/ProductServiceImp.java
@@ -1,10 +1,15 @@
 package com.empresa.product.service;
 
+import java.beans.FeatureDescriptor;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -25,8 +30,24 @@ public class ProductServiceImp implements ProductService {
 	HangarService hangarService;
 	
 	public void validateMandatoryFields(Product product) {
+		
 		if ( product != null && (product.getName() == null || "".equals(product.getName()) ))
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid field: name");
+		
+	}
+	
+	/*
+	 * Returns string array containing the property names of not-null properties of a product
+	 */
+	public String[] getNullPropertyNames(Product product) {
+		
+		final BeanWrapper wrappedProduct = new BeanWrapperImpl(product);
+		
+		return Stream.of(wrappedProduct.getPropertyDescriptors())
+				.map(FeatureDescriptor::getName)
+				.filter(propertyName -> wrappedProduct.getPropertyValue(propertyName) == null)
+				.toArray(String[]::new);
+		
 	}
 
 	@Override
@@ -51,23 +72,30 @@ public class ProductServiceImp implements ProductService {
 		
 		validateMandatoryFields(product);
 		
-		//Hangar Service comprueba la existencia del hangar
+		// Hangar Service comprueba la existencia del hangar
 		Hangar hangar = hangarService.getHangarById(product.getHangar().getId());
 		product.setHangar(hangar);
 		
 		return productDAO.save(product);
+		
 	}
 	
 	@Override
-	public Product editProduct(Long id, Product product) {
+	public Product editProduct(Long id, Product update) {
 		
 		if (!productDAO.existsById(id)) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found: invalid product ID");
 		}
 		else {
-		Product toUpdate = new Product(id, product.getName(), product.getDescription(), product.getQty(), product.getHangar());
-		return productDAO.save(toUpdate);
+			Product original = productDAO.getProductById(id);
+			
+			String[] nullPropertyNames = getNullPropertyNames(update);
+			BeanUtils.copyProperties(original, update, nullPropertyNames);
+			//https://stackoverflow.com/questions/45904389/spring-boot-how-to-edit-entity.
+			
+			return productDAO.save(original);
 		}
+		
 	}
 	
 	@Override
